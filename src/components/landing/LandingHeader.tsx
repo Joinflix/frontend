77a52253098d612e9b5a8e5e0ee8978f134a8a87
devYuraKim <dev.yurakim@gmail.com>
2026-01-23 13:07:50 +0900

    style(landing): 랜딩 페이지 헤더 Joinflix 글꼴 크기 및 간격 확장

--- src/components/landing/LandingHeader.tsx
+++ src/components/landing/LandingHeader.tsx
@@ -1,7 +1,7 @@
 const LandingHeader = () => {
   return (
     <header className="flex flex-row justify-between py-5 px-8">
-      <div className="text-[#816BFF] font-extrabold text-lg uppercase tracking-tight">
+      <div className="text-[#816BFF] font-extrabold text-xl uppercase tracking-wide">
         joinflix
       </div>
       <button className="bg-[#816BFF] cursor-pointer hover:bg-[#5e42c8] text-white text-xs rounded-xs px-4 py-1.5 flex items-center justify-center">
