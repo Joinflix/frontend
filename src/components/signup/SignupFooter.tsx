24686362cbaba5c5c7713f1ab044ec05b7d549f5
devYuraKim <dev.yurakim@gmail.com>
2026-01-26 11:27:06 +0900

    refactor(signup): signup 페이지 body, footer component로 분리

---
+++ src/components/signup/SignupFooter.tsx
@@ -0,0 +1,11 @@
+const SignupFooter = () => {
+  return (
+    <footer className="bg-gray-100 w-full flex-1">
+      <div className="mx-auto mt-10 font-extralight text-xs text-center">
+        signup footer
+      </div>
+    </footer>
+  );
+};
+
+export default SignupFooter;
