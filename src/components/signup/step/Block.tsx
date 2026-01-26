3442b4820495869bbf1c1377eb5adb9db5c6040a
devYuraKim <dev.yurakim@gmail.com>
2026-01-26 17:21:32 +0900

    style(signup): Plan column의 세로 간격 확장

--- src/components/signup/step/Block.tsx
+++ src/components/signup/step/Block.tsx
@@ -1,10 +1,10 @@
 const Block = ({ title, description }) => {
   return (
-    <>
+    <div className="pb-2">
       <div className="text-gray-500 text-xs">{title}</div>
       <div className="font-semibold text-sm text-gray-600">{description}</div>
       <div className="mt-2 border-b border-gray-300"></div>
-    </>
+    </div>
   );
 };
 
