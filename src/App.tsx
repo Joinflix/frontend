54e3666b5a692962401be1e491110a0b24e39bda
devYuraKim <dev.yurakim@gmail.com>
2026-01-27 16:36:01 +0900

    chore(TanStack): query 및 devtool 사용 활성화 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "react-router";
 import routes from "./router/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+      <TanStackDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), devtools()],
 });
