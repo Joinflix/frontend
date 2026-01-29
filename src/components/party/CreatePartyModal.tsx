86e9830324209e55d50d7364938fcf4881c7837b
devYuraKim <dev.yurakim@gmail.com>
2026-01-29 16:35:54 +0900

    feat(party): 파티 생성 시 컨페티 적용

--- src/components/party/CreatePartyModal.tsx
+++ src/components/party/CreatePartyModal.tsx
@@ -15,6 +15,7 @@ import type { PartyType } from "../../types/party";
 import { partySchema, type PartyFormValues } from "../../schemas/partySchema";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import confetti from "canvas-confetti";
 
 type CreatePartyModalProps = {
   partyOpen: boolean;
@@ -43,11 +44,52 @@ const CreatePartyModal = ({
     defaultValues: { name: "", password: "" },
   });
 
-  const onSubmit = (data: PartyFormValues) => {
+  const onSubmit = async (data: PartyFormValues) => {
     // /api/parties (POST)
     // *movieId, roomName, *isPublic, *hostControl, passCode
-    alert(data.name + " / " + data.password + " / " + data.hostControl);
+    fireConfetti();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // alert(data.name + " / " + data.password + " / " + data.hostControl);
     navigate(`/watch/party?title=${title}`);
+  };
+
+  const fireConfetti = () => {
+    const end = Date.now() + 0.15 * 1000;
+    const colors = ["#816BFF", "#FF3D81", "#00FFF0"];
+
+    const frame = () => {
+      if (Date.now() > end) return;
+
+      const randomColor = () =>
+        colors[Math.floor(Math.random() * colors.length)];
+
+      const config = {
+        particleCount: 10,
+        spread: 70,
+        startVelocity: 80,
+        ticks: 300,
+        gravity: 2.5,
+        scalar: 1.2,
+        shapes: ["square", "circle"],
+      };
+
+      confetti({
+        ...config,
+        angle: 60,
+        origin: { x: 0, y: 0.8 },
+        colors: [randomColor(), "#FFFFFF"],
+      });
+
+      confetti({
+        ...config,
+        angle: 120,
+        origin: { x: 1, y: 0.8 },
+        colors: [randomColor(), "#FFFFFF"],
+      });
+      requestAnimationFrame(frame);
+    };
+    frame();
   };
 
   return (
