9c7cf119ae086fa3ee7e3f05f81faa597b2d99f7
devYuraKim <dev.yurakim@gmail.com>
2026-01-30 09:18:45 +0900

    style(party): CreatePartyModal 디자인 1안

--- src/components/party/PartyTypeCard.tsx
+++ src/components/party/PartyTypeCard.tsx
@@ -21,35 +21,18 @@ const PartyTypeCard = ({
       className={`w-[50%] rounded-sm flex flex-col justify-center p-3 gap-1 cursor-pointer transition-all
         ${
           active
-            ? "border-4 border-[#E6E0FF] scale-105"
+            ? "border-1 border-[#816BFF] scale-105 bg-black"
             : "border border-[#816BFF]/30 opacity-60 hover:scale-105"
         }
       `}
-      style={
-        active
-          ? {
-              boxShadow: `
-                0 0 10px #FFF,
-                0 0 10px #816BFF,
-                inset 0 0 10px #816BFF
-              `,
-            }
-          : undefined
-      }
     >
       <div className="flex justify-center py-2">
         {React.cloneElement(icon as React.ReactElement, {
-          className: `size-20 transition-all duration-300 ${
+          className: `size-12 transition-all duration-300 ${
             active
-              ? "stroke-[#E6E0FF] stroke-[0.07rem]"
+              ? "stroke-white/70 stroke-[0.07rem]"
               : "stroke-gray-400 stroke-[0.09rem]"
           }`,
-          style: active
-            ? {
-                filter:
-                  "drop-shadow(0 0 2px #FFF) drop-shadow(0 0 6px #816BFF) drop-shadow(0 0 14px #816BFF)",
-              }
-            : undefined,
         })}
       </div>
 
