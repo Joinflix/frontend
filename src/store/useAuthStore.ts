9bd0b312959c4bafd2a89fcc5dab8a80be69e4ef
devYuraKim <dev.yurakim@gmail.com>
2026-02-03 11:44:36 +0900

    feat(auth): authStore에 user 정보 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,11 +1,29 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import { jwtDecode } from "jwt-decode";
+
+type JwtPayload = {
+  sub: string;
+  email: string;
+  role: string;
+  nickName: string;
+  iat: number;
+  exp: number;
+};
 
 type AuthState = {
   isAuthChecked: boolean;
   accessToken: string | null;
+  user: User | null;
   setAuth: (token: string | null) => void;
   clearAuth: () => void;
+};
+
+type User = {
+  userId: number;
+  email: string;
+  role: string;
+  nickName: string;
 };
 
 export const useAuthStore = create<AuthState>()(
@@ -13,14 +31,37 @@ export const useAuthStore = create<AuthState>()(
     (set) => ({
       isAuthChecked: false,
       accessToken: null,
-      setAuth: (token) =>
-        set({
-          accessToken: token,
-          isAuthChecked: true,
-        }),
+      user: null,
+      setAuth: (token) => {
+        if (!token) {
+          set({ accessToken: null, user: null, isAuthChecked: true });
+          return;
+        }
+
+        try {
+          const decoded = jwtDecode<JwtPayload>(token);
+
+          const user: User = {
+            userId: Number(decoded.sub),
+            email: decoded.email,
+            role: decoded.role,
+            nickName: decoded.nickName,
+          };
+
+          set({
+            accessToken: token,
+            user: user,
+            isAuthChecked: true,
+          });
+        } catch (error) {
+          console.error("Failed to decode token:", error);
+          set({ accessToken: null, user: null, isAuthChecked: true });
+        }
+      },
       clearAuth: () =>
         set({
           accessToken: null,
+          user: null,
           isAuthChecked: true,
         }),
     }),
