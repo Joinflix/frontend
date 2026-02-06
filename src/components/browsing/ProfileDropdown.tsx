f4eb1d246854e8b7a6b8af3f21e393011fafb6aa
devYuraKim <dev.yurakim@gmail.com>
2026-02-06 14:36:11 +0900

    style(header): 호버 시 색상 변경

--- src/components/browsing/ProfileDropdown.tsx
+++ src/components/browsing/ProfileDropdown.tsx
@@ -9,6 +9,8 @@ import {
 } from "../ui/dropdown-menu";
 import { LogOut, MessageCircleQuestionMark, UserRoundCog } from "lucide-react";
 
+const ICON_STYLE = "text-white group-hover:text-black";
+
 export const ProfileDropdown = ({ iconStyle }) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -21,20 +23,20 @@ export const ProfileDropdown = ({ iconStyle }) => {
         <DropdownMenuTrigger asChild>
           <div className={`${iconStyle} bg-white ml-2 rounded-sm`} />
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="bg-zinc-900/90 backdrop-blur-xl border border-white/10 text-white rounded-xl mt-2 min-w-[160px]">
+        <DropdownMenuContent className="bg-zinc-900/90 backdrop-blur-xl border border-white/10 text-white rounded-xl mt-2 min-w-[130px]">
           <DropdownMenuGroup>
-            <DropdownMenuItem className="cursor-pointer">
-              <UserRoundCog /> 프로필 관리
+            <DropdownMenuItem className="group cursor-pointer">
+              <UserRoundCog className={ICON_STYLE} /> 프로필 관리
             </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
-              <MessageCircleQuestionMark />
+            <DropdownMenuItem className="group cursor-pointer">
+              <MessageCircleQuestionMark className={ICON_STYLE} />
               고객센터
             </DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator className="bg-white/5" />
           <DropdownMenuGroup>
-            <DropdownMenuItem className="cursor-pointer text-white focus:text-black">
-              <LogOut />
+            <DropdownMenuItem className="group cursor-pointer text-white focus:text-black">
+              <LogOut className={ICON_STYLE} />
               로그아웃
             </DropdownMenuItem>
           </DropdownMenuGroup>
