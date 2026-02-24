951f763dc8d06e8b565ee789ceb9d9177ae57173
devYuraKim <dev.yurakim@gmail.com>
2026-02-24 15:17:33 +0900

    fix(routes): PENDING 상태 사용자 멤버십 결제 페이지로 리다이렉트

--- src/router/AuthenticatedRoutes.tsx
+++ src/router/AuthenticatedRoutes.tsx
@@ -1,11 +1,14 @@
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import { useNotification } from "../hooks/useNotification";
 import SpinnerPage from "../pages/SpinnerPage";
 import { useAuthStore } from "../store/useAuthStore";
 
 const AuthenticatedRoutes = () => {
+  const location = useLocation();
+
   const accessToken = useAuthStore((state) => state.accessToken);
   const isAuthChecked = useAuthStore((state) => state.isAuthChecked);
+  const userStatus = useAuthStore((state) => state.user?.status);
 
   useNotification();
 
@@ -15,6 +18,16 @@ const AuthenticatedRoutes = () => {
 
   if (!accessToken) {
     return <Navigate to="/signin" replace />;
+  }
+
+  const isOnboardingRoute = location.pathname.startsWith("/signup/step4");
+
+  if (userStatus === "PENDING" && !isOnboardingRoute) {
+    return <Navigate to="/signup/step4-list-membership" replace />;
+  }
+
+  if (userStatus === "ACTIVE" && isOnboardingRoute) {
+    return <Navigate to="/browsing" replace />;
   }
 
   return <Outlet />;
